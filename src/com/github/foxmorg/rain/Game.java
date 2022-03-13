04f3f709d22066a15a74423cd6d6fbe50b4b27ca
foxmorg <foxmorg@gmail.com>
2022-03-13 20:40:08 +0200

    episode 006 - graphics initialized

--- src/com/github/foxmorg/rain/Game.java
+++ src/com/github/foxmorg/rain/Game.java
@@ -55,6 +55,12 @@ public class Game extends Canvas implements Runnable{
             createBufferStrategy(3);
             return;
         }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
