4249dbfa864bc9bd1a8a346ea1d2cdcb85b8b41f
foxmorg <foxmorg@gmail.com>
2022-08-07 11:10:14 +0300

    episode 038 - procedural rendering

--- src/com/github/foxmorg/rain/level/Level.java
+++ src/com/github/foxmorg/rain/level/Level.java
@@ -36,10 +36,10 @@ public class Level {
 
     public void render(int xScroll, int yScroll, Screen screen) {
         screen.setOffset(xScroll, yScroll);
-        int x0 = xScroll >> 4; // divide by 16
-        int x1 = (xScroll + screen.width) >> 4;
-        int y0 = yScroll >> 4;
-        int y1 = (yScroll + screen.height) >> 4;
+        int x0 = (xScroll >> 4) - 1; // divide by 16
+        int x1 = ((xScroll + screen.width) >> 4) + 1;
+        int y0 = (yScroll >> 4) - 1;
+        int y1 = ((yScroll + screen.height) >> 4) + 1;
 
         for (int y = y0; y < y1; y++) {
             for (int x = x0; x < x1; x++) {
