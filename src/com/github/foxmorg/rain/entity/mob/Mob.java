b4757dc330d24db54d5fc97483273aa6111669ab
foxmorg <foxmorg@gmail.com>
2022-08-07 11:53:17 +0300

    episode 042 - the move method

--- src/com/github/foxmorg/rain/entity/mob/Mob.java
+++ src/com/github/foxmorg/rain/entity/mob/Mob.java
@@ -9,8 +9,17 @@ public abstract class Mob extends Entity {
     protected int dir = 0; // possible directions: 0 - north, 1 - east, 2 - south, 3 - west
     protected boolean moving = false;
 
-    public void move() {
+    public void move(int xa, int ya) {
+        if (xa > 0) dir = 1;
+        if (xa < 0) dir = 3;
+        if (ya > 0) dir = 2;
+        if (ya < 0) dir = 0;
 
+        if (!collision()) {
+            // -1, 0, 1
+            x += xa;
+            y += ya;
+        }
     }
 
     public void update() {
