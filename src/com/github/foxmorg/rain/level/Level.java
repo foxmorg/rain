304e16a1b5a2c83be72481284ed7266068b5c5ba
foxmorg <foxmorg@gmail.com>
2022-08-07 10:03:53 +0300

    episode 033 - the void tile

--- src/com/github/foxmorg/rain/level/Level.java
+++ src/com/github/foxmorg/rain/level/Level.java
@@ -43,6 +43,6 @@ public class Level {
 
     public Tile getTile(int x, int y) {
         if (tiles[x + y * width] == 0) return Tile.grass;
-        return null;
+        return Tile.voidTile;
     }
 }
--- src/com/github/foxmorg/rain/level/tile/VoidTile.java
+++ src/com/github/foxmorg/rain/level/tile/VoidTile.java
@@ -3,14 +3,19 @@ package com.github.foxmorg.rain.level.tile;
 import com.github.foxmorg.rain.graphics.Screen;
 import com.github.foxmorg.rain.graphics.Sprite;
 
-public class GrassTile extends Tile {
+public class VoidTile extends Tile {
 
-    public GrassTile(Sprite sprite) {
+    public VoidTile(Sprite sprite) {
         super(sprite);
     }
 
     @Override
     public void render(int x, int y, Screen screen) {
         screen.renderTile(x, y, this);
+    }
+
+    @Override
+    public boolean solid() {
+        return true;
     }
 }
